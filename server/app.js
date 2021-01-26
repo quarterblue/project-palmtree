7f34384d9e0f153f6a00806d3740e22132f4d4aa
quarterblue <snowmanis911@gmail.com>
2021-01-25 21:55:58 -0800

    Added env variables and initial API endpoint

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const companyinfo = require('./seeds/companyinfo');
+
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Project Palmtree!');
+  res.send('Welcome to Project Palmtree API!');
+});
+
+app.get('/api/v1/stocks/:symbol', (req, res) => {
+  res.send({
+    stock: 'API END POINT',
+    // name: companyinfo[req.params.symbol].name,
+  });
 });
 
 app.listen(PORT, () => {
