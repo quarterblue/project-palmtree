4abfac8ef00ace2e101c36771fa734165c8f9a62
glo <lo.granton2000@gmail.com>
2021-01-26 21:39:38 -0800

    added title

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Typography from '@material-ui/core/Typography';
 import './index.css';
+
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Kanit',
+      'sans-serif',
+    ].join(','),
+  },});
+
+function Title() {
+    return (
+        <ThemeProvider theme={theme}>
+            <div id="title">
+                <Typography id="title">
+                    Palmtree
+                </Typography>  
+            </div>
+        </ThemeProvider>
+
+    );
+}
+
+ReactDOM.render(
+    <Title/>,
+    document.getElementById('root')
+  );
