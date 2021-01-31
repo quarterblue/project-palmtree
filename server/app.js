506a1aaeaa060a74e91d1ad3a91cce9dd666f820
quarterblue <snowmanis911@gmail.com>
2021-01-31 00:40:15 -0800

    Allow data fetching from the database

--- server/app.js
+++ server/app.js
@@ -4,15 +4,13 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const companyinfo = require('./seeds/companyinfo');
 const connectDB = require('./configs/connectDB');
+const Stock = require('./models/stock');
 
 dotenv.config();
-
-connectedDB();
+connectDB();
 
 const db = mongoose.connection;
-
 const app = express();
-
 const PORT = process.env.API_PORT || 5000;
 
 app.use(morgan('tiny'));
@@ -20,13 +18,16 @@ app.get('/', (req, res) => {
   res.send('Welcome to Project Palmtree API!');
 });
 
+// REST API: GET stock info by symbol
 app.get('/api/v1/stocks/:symbol', (req, res) => {
-  const stockSymbol = req.params.symbol;
-  res.send({
-    type: 'API END POINT',
-    stock: req.params.symbol,
-    // name: companyinfo[req.params.symbol].name,
-  });
+  const stockSymbol = req.params.symbol.toUpperCase();
+  const stockInfo = Stock.findOne({ Symbol: stockSymbol })
+    .then((stock) => {
+      res.send(stock);
+    })
+    .catch((e) => {
+      res.send('This stock symbol does not exist!');
+    });
 });
 
 app.listen(PORT, () => {
